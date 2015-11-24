4145875141137ca0ade8602887fa212b12971b2a
Andrew Levin <anlevin@Andrews-MacBook-Pro.local>
2015-11-24 00:23:42 -0500

    secondCommit

--- js/main.js
+++ js/main.js
@@ -3,11 +3,19 @@ var socket = io();
 $(document).ready(function(){
 	console.log('document ready');
 	$('#button').click(function(){
-			socket.emit('button clicked', $('#firstNameField').val());
+		var msg=
+		{
+  			first: $('#firstNameField').val(),
+  			last: $('#lastNameField').val(),
+  			message: $('#messageField').val()
+		}
+			
+		socket.emit('button clicked', msg);
 	});
 
 	socket.on('button was clicked', function(msg){
-		console.log('button was clicked, message received');
+		console.log(msg.first+ ' '+ msg.last+ ' clicked, message received');
+		$('#chat').append('<li>'+msg.message+'</li>');
 	});
 });
 
