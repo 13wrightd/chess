510b194292bd34d7b0b174795f34c784e0c2c309
Andrew Levin <anlevin@Andrews-MacBook-Pro.local>
2015-11-24 19:08:26 -0500

    thirdCommit

--- js/main.js
+++ js/main.js
@@ -2,16 +2,33 @@ var socket = io();
 
 $(document).ready(function(){
 	console.log('document ready');
+
+
+		
 	$('#button').click(function(){
-		var msg=
-		{
-  			first: $('#firstNameField').val(),
-  			last: $('#lastNameField').val(),
-  			message: $('#messageField').val()
-		}
-			
+	var msg=
+	{
+  		first: $('#firstNameField').val(),
+  		last: $('#lastNameField').val(),
+  		message: $('#messageField').val()
+	}
+  		
 		socket.emit('button clicked', msg);
 	});
+
+	$('#messageField').keypress(function(event){
+	var msg=
+	{
+  		first: $('#firstNameField').val(),
+  		last: $('#lastNameField').val(),
+  		message: $('#messageField').val()
+	}
+  		
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if(keycode == '13'){
+			socket.emit('button clicked', msg);
+		}
+	})
 
 	socket.on('button was clicked', function(msg){
 		console.log(msg.first+ ' '+ msg.last+ ' clicked, message received');
